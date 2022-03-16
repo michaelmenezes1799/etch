a527bd94ffc1180e85edf7efdf7c7e1b9d6d61c7
Michael <michaelmenezes1799@gmail.com>
2022-03-15 21:35:47 -0400

    added painting function on mouse hover. Added Reset button that cleans the board and asks for new size.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+
+
+reset();
+
+//function holding the whole show together to be reset when you hit the reset button. might be a better way to structure this.
+function reset (e) {
+
 let gridLength = prompt("Enter the desired length of one side. Min: 4 Max: 100");
 console.log(gridLength);
 
@@ -10,16 +17,36 @@ while (gridLength < 4 || gridLength > 100) {
 for (let i = 0; i < (gridLength * gridLength); i++){
     console.log("test");
     const container = document.getElementById("container");
-    let gridSquare = document.createElement("div");
+    const gridSquare = document.createElement("div");
     gridSquare.classList.add("gridSquare");
+    
+    //adds the event that uses the paint function when a gridsquare is moused over
+    gridSquare.addEventListener("mouseover", paintWhite);
     container.appendChild(gridSquare);
+    
+    const button = document.getElementById("reset");
+    button.addEventListener("click", reset);
+    button.addEventListener("click", clean)
+   
+    //adds a class that paints the moused over gridsquare white
+    function paintWhite (e) {
+        gridSquare.classList.add("white");
+
+    }
+    //cleans the board when you reset.
+    function clean (e) {
+        gridSquare.classList.remove("white");
+    }
+    
 }
     //sets the grid dimensions for the etch-a-sketch container
     container.style.gridTemplateColumns = `repeat(${gridLength}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridLength}, 1fr)`;
-    
 
-    
+}
+
+
+
 
     
     
